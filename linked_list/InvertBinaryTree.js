19a71fc9d32b945610df76534d4b9901c524061d
ThiagoDe <17tawi@gmail.com>
2022-04-21 18:58:26 -0700

    Depth Tree

--- linked_list/InvertBinaryTree.js
+++
@@ -1,7 +0,0 @@
-const inverTree = (root) => {
-    if (root === null) return null;
-    const left = root.left;
-    const right = root.right;
-    root.left = inverTree(right);
-    root.right = inverTree(left);
-}
