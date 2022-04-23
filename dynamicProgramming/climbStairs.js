cc214245ce436f737290aa4c1751dd671c34f1d4
ThiagoDe <17tawi@gmail.com>
2022-04-23 13:33:54 -0700

    dynamic Programming

---
+++ dynamicProgramming/climbStairs.js
@@ -0,0 +1,16 @@
+//Not working
+
+var climbStairs = function(n, store = {}) {
+    if (n === 0) return 1;
+    if (n < 0) return 0;
+    if (n in store) return store[n];
+    store[n - 2] = climbStairs(n - 2);
+    store[n - 1] = climbStairs(n - 1);
+    
+    store[n] = store[n - 2] + store[n - 1]
+    // console.log(store)
+    return store[n];
+    
+};
+
+console.log(climbStairs(3))
