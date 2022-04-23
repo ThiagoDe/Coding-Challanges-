22a993cafe5a8cbc258d08410861ee6650543d9e
ThiagoDe <17tawi@gmail.com>
2022-04-23 13:49:39 -0700

    fixed memo climbing stairs

--- dynamicProgramming/climbStairs.js
+++ dynamicProgramming/climbStairs.js
@@ -4,8 +4,8 @@ var climbStairs = function(n, store = {}) {
     if (n === 0) return 1;
     if (n < 0) return 0;
     if (n in store) return store[n];
-    store[n - 2] = climbStairs(n - 2);
-    store[n - 1] = climbStairs(n - 1);
+    store[n - 2] = climbStairs(n - 2, store);
+    store[n - 1] = climbStairs(n - 1, store);
     
     store[n] = store[n - 2] + store[n - 1]
     // console.log(store)
