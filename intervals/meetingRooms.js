7960f48ea3e35494615fdb00a5f2cd7300e90878
ThiagoDe <17tawi@gmail.com>
2022-04-24 22:03:56 -0700

    v2

---
+++ intervals/meetingRooms.js
@@ -0,0 +1,20 @@
+
+//   class Interval {
+//     constructor(start, end) {
+//       this.start = start;
+//       this.end = end;
+//     }
+//   }
+ 
+
+
+ const canAttendMeetings = (intervals) => {
+    
+  }
+
+
+const intervals = [[0,30),(5,10),(15,20)];
+console.log(canAttendMeetings(intervals))
+// Output: false
+// Explanation: 
+// (0,30), (5,10) and (0,30),(15,20) will conflict
