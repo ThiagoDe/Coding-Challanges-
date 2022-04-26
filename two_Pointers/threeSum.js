eb1b4e697ab88d00d86c520f6a61c658c85f9907
ThiagoDe <17tawi@gmail.com>
2022-04-26 00:57:27 -0700

    Three Sum v1

--- two_Pointers/threeSum.js
+++ two_Pointers/threeSum.js
@@ -1,10 +1,27 @@
 const threeSum = (nums) => {
     let results = [];
-    let sorted = nums.sort((a, b) => a - b);
-    let i = 0;
-    let j = nums.length - 1;
-    while (){}
-       let target = sorted[i]
+    let sorted = nums.sort((a, b) => a - b); // [ -4, -1, -1, 0, 1, 2 ]
+
+    for (let i = 0; i < nums.length; i++){
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+        const target = 0 - nums[i];
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (right > left) {
+            let sum = nums[left] + nums[right];
+            if (sum > target){
+                right -= 1;
+            } else if (sum < target){
+                left += 1;
+            } else {
+                results.push([ nums[i], nums[left], nums[right]])
+                while (nums[left] === nums[left + 1]) left++;
+                while (nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+        }
     }
     return results;
 }
